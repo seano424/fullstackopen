91dd263613cc34f25576aba14e3b8745f3d5a0de
Sean OReilly <soreilly424@gmail.com>
2021-06-01 13:21:56 -0500

    1.4

--- part1/courseinfo/src/components/Total.js
+++ part1/courseinfo/src/components/Total.js
@@ -1,5 +1,10 @@
 import React from "react";
 
-export default function Total({ exercises1, exercises2, exercises3 }) {
-  return <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>;
+export default function Total({ parts }) {
+  return (
+    <p>
+      Number of exercises{" "}
+      {parts[0].exercises + parts[1].exercises + parts[2].exercises}
+    </p>
+  );
 }
